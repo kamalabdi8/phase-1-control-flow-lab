ba11436e0d7766f6ebbf69756b4116e0143cd414
kamalabdi8 <kamalabdi042@gmail.com>
2024-09-24 11:48:50 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return "This one is on me!"
+  }else if (feet>=400&&feet<=2000){
+    return "That will be twenty bucks."
+  }else if (feet>2000&&feet<=2500){
+    return "I will gladly take your thirty bucks."
+  }else  {
+    return "No can do."
+  }
 }
+function ternaryCheckCity(city){
+  if (city == "NYC"){
+    return "Ok, sounds good.";
+  }else if (city !== "NYC"){
+    return "No go."
+  }
+}
+  function switchOnCharmFromTip(Tip){
+switch (Tip) {
+case "generous":
+  return "Thank you so much.";
+  case "not as generous":
+    return "Thank you.";
+    default:
+      return "Bye.";
+}
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+  
+  
